87d4a3b9e3ca6601b1d3f9535fcb0d76e162a276
Kwon yongho <b2b2004@nate.com>
2023-09-26 18:24:29 +0900

    #1 - SpringBoot Setting
    SpringBoot 2.7.16 버전 사용

---
+++ SI-Track-Backend/src/main/java/com/sitrack/sitrackbackend/SiTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sitrack.sitrackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiTrackBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiTrackBackendApplication.class, args);
+    }
+
+}
---
+++ SI-Track-Backend/src/test/java/com/sitrack/sitrackbackend/SiTrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitrack.sitrackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiTrackBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
