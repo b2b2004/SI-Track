416ad0f68b2548fae95cb99f8e9480cd4ad7147e
JungKyungJin <rudwls512@naver.com>
2023-10-20 13:10:35 +0900

    #24 - React 초기 설정
    React.js 초기 설정 및 View 초기 설정 완료

---
+++ SI-Track-Frontend/SiSi-master/src/components/App.js
@@ -0,0 +1,12 @@
+import Footer from "./Footer"
+import Header from './Header';
+
+export default function App({children}){
+    return(
+        <div id="wrap">
+            <Header />
+            <div>{children}</div>
+            <Footer />
+            </div>
+    )
+}
