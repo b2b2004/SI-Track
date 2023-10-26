fd14f0396658f6e417cc4039e86a5a8c783793a5
Kwon yongho <b2b2004@nate.com>
2023-10-26 20:05:31 +0900

    #40 - 이미지 관련 @RequestPart 사용
    다중 이미지와 json 형식의 Request(Dto)를 함께 받기 때문에 @ReqeustPart로 변경하였다.

--- SI-Track-Backend/src/main/java/com/sitrack/sitrackbackend/controller/ProductController.java
+++ SI-Track-Backend/src/main/java/com/sitrack/sitrackbackend/controller/ProductController.java
@@ -18,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
 import java.util.List;
@@ -32,27 +33,38 @@ public class ProductController {
     private final PaginationService paginationService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> register_product(@RequestBody ProductRequest productRequest, @AuthenticationPrincipal PrincipalDetails principalDetails){
-        String msg = productService.register_product(productRequest.toDto(principalDetails.todto()), productRequest.productImages());
+    public ResponseEntity<?> register_product(@RequestPart ProductRequest productRequest,
+                                              @RequestPart List<MultipartFile> productImages,
+                                              @AuthenticationPrincipal PrincipalDetails principalDetails){
+        String msg = productService.register_product(productRequest.toDto(principalDetails.todto()), productImages);
         return new ResponseEntity<>(msg, HttpStatus.OK);
     }
 
+    @GetMapping("/update/{productId}")
+    public ResponseEntity<?> update_form(@PathVariable Long productId){
+        ProductResponse product = productService.findbyId_product_one(productId);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
     @PostMapping("/update/{productId}")
-    public ResponseEntity<?> update_product(@RequestBody ProductUpdateRequest productUpdateRequest, @PathVariable Long productId, @AuthenticationPrincipal PrincipalDetails principalDetails){
-        String msg = productService.update_product(productId, productUpdateRequest.toDto(principalDetails.todto()));
+    public ResponseEntity<?> update_product(@RequestPart ProductUpdateRequest productUpdateRequest,
+                                            @RequestPart List<MultipartFile> productImages,
+                                            @PathVariable Long productId,
+                                            @AuthenticationPrincipal PrincipalDetails principalDetails){
+        String msg = productService.update_product(productId, productUpdateRequest.toDto(principalDetails.todto()), productImages);
         return new ResponseEntity<>(msg, HttpStatus.OK);
     }
 
     @DeleteMapping("{productId}")
-    public ResponseEntity<?> delete_product(@PathVariable Long productId, @AuthenticationPrincipal PrincipalDetails principalDetails){
+    public ResponseEntity<?> delete_product(@PathVariable Long productId,
+                                            @AuthenticationPrincipal PrincipalDetails principalDetails){
         String msg = productService.delete_product(productId, principalDetails.getUser().getUserId());
         return new ResponseEntity<>(msg, HttpStatus.OK);
     }
 
     @GetMapping("/list")
-    public ResponseEntity<?> find_all_and_search(
-            @PageableDefault(size = 12, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            @RequestParam(required = false) String searchValue){
+    public ResponseEntity<?> find_all_and_search(@PageableDefault(size = 12, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+                                                 @RequestParam(required = false) String searchValue){
         Map<String, Object> result = new HashMap<>();
         Page<ProductResponse> products = productService.findbyId_product_all_and_search(pageable, searchValue).map(ProductResponse::from);
         List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), products.getTotalPages());
