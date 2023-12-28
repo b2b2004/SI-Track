4be291ec694d2acf72349e7642c3f1f762b242db
Kwon yongho <b2b2004@nate.com>
2023-12-28 18:40:37 +0900

    #87 - RefreshToken을 활용 AccessToken 재발급
    기존 AccessToken이 만료 될 시 해당 컨트롤러 주소로 만료된 AccessToken를 넘기면 AccessToken으로 Redis에 RefreshToken이 있는지 확인 -> AccessToken 재발급

--- SI-Track-Backend/src/main/java/com/sitrack/sitrackbackend/controller/AccountController.java
+++ SI-Track-Backend/src/main/java/com/sitrack/sitrackbackend/controller/AccountController.java
@@ -3,20 +3,22 @@ package com.sitrack.sitrackbackend.controller;
 import com.sitrack.sitrackbackend.config.security.auth.PrincipalDetails;
 import com.sitrack.sitrackbackend.domain.account.UserAccount;
 import com.sitrack.sitrackbackend.dto.*;
+import com.sitrack.sitrackbackend.dto.request.TokenReqeust;
 import com.sitrack.sitrackbackend.dto.response.SearchIdResponse;
 import com.sitrack.sitrackbackend.service.AccountService;
 import com.sitrack.sitrackbackend.service.EmailService;
+import com.sitrack.sitrackbackend.service.TokenService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 
 @RequiredArgsConstructor
 @RequestMapping("/account")
@@ -24,6 +26,7 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final AccountService accountService;
+    private final TokenService tokenService;
 
 //    @PostMapping("/login")
 //    public ResponseEntity<?> userAccountLogin(@Valid @RequestBody LoginDto loginDto){
@@ -77,4 +80,16 @@ public class AccountController {
         return new ResponseEntity<>(user1, HttpStatus.OK);
     }
 
+    /**
+     *  RequestHeader로 받을 시 Spring Security Filter에 걸려
+     *  ExpiredJwtException 오류가 나서 RequestBody로 진행..
+     */
+    @GetMapping("/user/reissue/token")
+    public ResponseEntity<?> reissueToken(@RequestBody TokenReqeust tokenReqeust){
+        String newToken = tokenService.validatateRefreshToken(tokenReqeust.authentication());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + newToken);
+        return new ResponseEntity<>("토큰 발급 성공" ,headers ,HttpStatus.OK);
+    }
 }
